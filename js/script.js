fa0e40d077e24ceae79a874f4f5536c1c637c425
shrutimary15 <shanshru15@gmail.com>
2023-07-31 18:09:35 +0530

    New Themw

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ======================== Typing Animation ========================  */
 var typed=new Typed(".typing", {
-    strings:["Machine Learning Engineer....", "Computer Vision Engineer....","Data Analyst....","NLP Expert....","Freelancer"],
+    strings:["Machine Learning Engineer....","Computer Vision Engineer....","Data Analyst....","NLP Expert....","Freelancer...."],
     typeSpeed:60,
     BackSpeed:30,
     loop:true
@@ -110,3 +110,36 @@ function downloadResume() {
     link.click();
     document.body.removeChild(link);
 }
+
+
+const textInput = document.getElementById("input-textarea")
+const textArea = document.getElementById("textarea")
+
+textInput.addEventListener("input", function () {
+    textInput.value = textArea.value;
+});
+
+
+
+/* ======================== Portfolio Image Github Link ========================  */
+
+const portfolioItems = document.querySelectorAll('.portfolio-item');
+
+// Add a click event listener to each portfolio item
+portfolioItems.forEach(item => {
+    // Find the image element and link element within each portfolio item
+    const image = item.querySelector('.portfolio-img img');
+    const link = item.querySelector('a');
+
+    // Add a click event listener to the image
+    image.addEventListener('click', () => {
+        // Get the URL from the link and open it in a new tab/window
+        const url = link.getAttribute('href');
+        if (url) {
+            window.open(url, '_blank');
+        }
+    });
+});
+
+
+
